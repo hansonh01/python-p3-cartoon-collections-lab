6ac2cdc9e8be3b4500dd6956e4a50e48e400eba3
Hanson <hanson.hua903@gmail.com>
2023-11-04 11:44:10 -0500

    Complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for index,dwarve in enumerate(dwarves,start=1):
+        print(f"{index}. {dwarve}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    calls = []
+    for element in planeteer_calls:
+        calls.append(element.capitalize() + "!")
+    return calls
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(lst):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    for items in lst:
+        if items in cheese_types:
+            return items
+    return None
